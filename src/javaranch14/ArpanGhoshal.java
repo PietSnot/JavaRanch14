10390b0a5644596617f6f490127b1635fb219cb7
Piet Snot <pietmuis@gmail.com>
2018-03-28 00:16:52 +0200

    JavaRanch14 - hulpprogrammaatjes

--- src/javaranch14/ArpanGhoshal.java
+++ src/javaranch14/ArpanGhoshal.java
@@ -7,8 +7,11 @@ package javaranch14;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -20,14 +23,21 @@ public class ArpanGhoshal {
 
 class test {
 
-    <T> void meth(T a) {
-        System.out.println("T " + a);
-    }
+//<T extends Collection<?>> void meth(T a) {
+//    System.out.println("T " + a.size());
+//}
 
-    void meth(Collection<?> a) {
-        System.out.println("Collection: " + a);
-    }
-    
+void meth(Collection<?> a) {
+    System.out.println("T " + a.size());
+}
+
+//    void meth(List<?> a) {
+//        System.out.println("List: " + a);
+//    }
+
+//    void meth(Collection<String> a) {
+//        System.out.println("Collection: " + a);
+//    }
 }
 
 class MyClass {
@@ -36,12 +46,15 @@ class MyClass {
 
         test ref = new test();
 
-        List<String> ob = new ArrayList<>();
+        Collection<String> ob = new ArrayList<>();
 
         ob.add("String 1");
         ob.add("String 2");
         ob.add("String 3");
 
         ref.meth(ob);
+        
+        Set<Point> set = new HashSet<>(Arrays.asList(new Point(), new Point()));
+        ref.meth(set);
     }
 }
